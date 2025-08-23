325c9e0270edf581a47294aed719df140d0d2003
RaghavGollapinni <gsrinivas.edu@gmail.com>
2025-08-24 04:15:44 +0530

    11

--- test.py
+++
@@ -1,7 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['en'])  # load English
-results = reader.readtext('sample2.jpg')
-
-for (bbox, text, prob) in results:
-    print(f"Detected text: {text} (confidence: {prob:.2f})")
